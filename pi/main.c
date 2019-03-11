da36b9a53092b03766b7b076375d60ac187960e1
dunmerihor <48093025+dunmerihor@users.noreply.github.com>
2019-03-11 21:43:58 +0700

    Update main.c

--- pi/main.c
+++ pi/main.c
@@ -8,14 +8,12 @@ int main() {
     int j = -1;
     double slag = 0;
     double pi = 4;
-    double pi2;
     do {
-        pi2 = pi;
         slag = 4. / i;
         pi = pi + j * slag;
         j = j * (-1);
         i = i + 2;
-    } while (((pi2 - pi) * j) > pow(10, (-(n+1))));
+    } while (slag > pow(10, (-(n+1))));
     printf("%.*f\n", n, pi);
     scanf("%d", &n);
     return 0;
